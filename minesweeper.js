143f07120b5ea851a29a853b47c44851e52b89cc
Le Pham <phamtle724@gmail.com>
2023-03-30 11:27:42 -0700

    add make board function

--- minesweeper.js
+++ minesweeper.js
@@ -10,12 +10,13 @@
  *
  * one class for a game
  *  - constructor:  height & width,
- *                  gameOver(boolean)
+ *                  gameOver(boolean),
+ *                  *nums of bombs
 
- *  - methods:      findSpot
- *                  handleClick function (needs binding),
- *                  makeBoard function,
+ *  - methods:      makeBoard function,
  *                  makeHTMLBoard function,
+ *                  findSpot
+ *                  handleClick function (needs binding),
  *                  checkForLoss/checkForWin?
  *                  endGame
 
@@ -26,3 +27,40 @@
  *
  */
 
+class Game {
+  constructor(height = 10, width = 10) {
+    this.height = height;
+    this.width = width;
+    this.gameOver = false;
+
+  }
+  /** makeBoard: create in-JS board structure
+   * board = array of rows. each row is array of tiles (board[y][x])
+   */
+  makeBoard() {
+    this.board = [];
+    for (let y = 0; y < this.height; y++) {
+      this.board.push(Array.from({ length: this.width }));
+    }
+  }
+
+  /** makeHTMLBoard: make HTML table and row of column tops */
+
+  makeHTMLBoard() {
+    const board = document.getElementById('board');
+    board.innerHTML = ' ';
+
+    for (let y = 0; y < this.height; y++) {
+      const row = document.createElement('tr');
+
+      for (let x = 0; x < this.width; x++) {
+        const tile = document.createElement('td');
+        tile.setAttribute('id', `${y}-${x}`);
+        tile.addEventListener('click', this.handleClick);
+        row.append(tile);
+      }
+      board.append(row);
+    }
+  }
+
+}
